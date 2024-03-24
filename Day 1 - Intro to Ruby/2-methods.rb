c26d0a5c63e92d48db5aaf8a55ce6c9ae4f1d775
Gianluca Romeo <gianlucaromeo@outlook.com>
2024-03-24 12:56:39 +0100

    Day 1 - Added procs and lambdas

--- Day 1 - Intro to Ruby/2-methods.rb
+++ Day 1 - Intro to Ruby/2-methods.rb
@@ -37,7 +37,6 @@ end
 def method_with_multiple_return_values
     return 1, 2
 end
-v1, v2 = method_with_multiple_return_values
 
 # Method with implicit return
 def method_with_implicit_return
@@ -61,3 +60,39 @@ end
 method_with_optional_block do
     puts "Inside Block"
 end
+
+# Procs
+# - Procs are blocks that are wrapped in a Proc object
+# - They can be stored in variables and passed around
+# - They can be called with the "call" method
+# - The difference between Procs and blocks is that Procs 
+#   can be called outside the method where they were defined
+my_proc = Proc.new { puts "Hello from Proc" }
+my_proc.call
+
+# Lambdas
+# - Lambdas are similar to Procs but with some differences
+# - They enforce the number of arguments passed to them
+# - They return control to the calling method
+my_lambda = -> { puts "Hello from Lambda" }
+my_lambda.call
+
+# Methods can be called with or without parentheses
+method_with_parameters "Gianluca", 24
+method_with_parameters("Gianluca", 24)
+
+method_with_default_parameters
+method_with_default_parameters "Gianluca"
+
+method_with_splat_operator 1, 2, 3, 4
+
+method_with_keyword_arguments name: "Gianluca", age: 24
+
+v1, v2 = method_with_multiple_return_values
+
+puts method_with_return_value
+
+puts v1
+puts v2
+
+puts method_with_implicit_return
